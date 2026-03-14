a2b8deb4b7c548a0c1c668f262eed5d8eaf2ec47
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-14 04:34:35 -0700

    mother penguin star pos

--- tweaks.lua
+++ tweaks.lua
@@ -1,3 +1,4 @@
 gLevelValues.entryLevel = LEVEL_CASTLE_GROUNDS
 gLevelValues.coinsRequiredForCoinStar = 80
 gLevelValues.exitCastleWarpNode = 16
+vec3f_set(gLevelValues.starPositions.TuxieMotherStarPos, 358, -5329, 9375)
