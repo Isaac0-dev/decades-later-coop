8a8d294fc8857a9f02afe296e4cf18e62e7a2085
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-14 05:07:06 -0700

    fixed pss star

--- tweaks.lua
+++ tweaks.lua
@@ -1,4 +1,6 @@
 gLevelValues.entryLevel = LEVEL_CASTLE_GROUNDS
 gLevelValues.coinsRequiredForCoinStar = 80
 gLevelValues.exitCastleWarpNode = 16
+gLevelValues.pssSlideStarTime = 840
+vec3f_set(gLevelValues.starPositions.PssSlideStarPos, 15116, -7303, 200) --Pos may be slightly off but fixed
 vec3f_set(gLevelValues.starPositions.TuxieMotherStarPos, 358, -5329, 9375)
