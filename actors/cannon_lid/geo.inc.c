d175cbbf7b86d20da2a8bb7cfc7e6b11a392496b
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-17 12:22:39 -0700

    cannon lid fixed

---
+++ actors/cannon_lid/geo.inc.c
@@ -0,0 +1,7 @@
+const GeoLayout cannon_lid_geo[] = {
+   GEO_NODE_START(),
+   GEO_OPEN_NODE(),
+       GEO_DISPLAY_LIST(LAYER_OPAQUE, cannon_lid_seg8_dl_080048E0),
+   GEO_CLOSE_NODE(),
+   GEO_END(),
+};
