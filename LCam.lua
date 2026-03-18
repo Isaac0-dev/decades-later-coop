ea12f72302f35f92758ae17e57dd502800f524d8
Fearl <40965214+Fearlv@users.noreply.github.com>
2026-03-18 08:07:57 -0500

    add L Camera Trigger
    - L Button Triggers Camera Mode while Flying or Swimming (only romhack camera)

---
+++ LCam.lua
@@ -0,0 +1,28 @@
+-- name: L Trigger
+-- description: L Button Triggers Camera Mode while Flying or Swimming
+
+local L_Cam = false
+
+---@param m MarioState
+local function mario_update(m)
+
+    if camera_config_is_free_cam_enabled() then return end
+    if m.playerIndex ~= 0 then return end
+
+    local camMode = gLakituState.mode
+    local swimFly = (m.action & ACT_FLAG_SWIMMING_OR_FLYING) ~= 0
+    local lPressed = (m.controller.buttonPressed & L_TRIG) ~= 0
+    local isCamMode = (camMode == CAMERA_MODE_BEHIND_MARIO or camMode == CAMERA_MODE_WATER_SURFACE)
+
+    if lPressed and swimFly then
+        L_Cam = not L_Cam
+    end
+
+    if isCamMode and (
+        (L_Cam and swimFly) or
+        (not swimFly and m.action == ACT_EMERGE_FROM_PIPE)) then
+        gLakituState.mode = CAMERA_MODE_ROM_HACK
+    end
+end
+
+hook_event(HOOK_MARIO_UPDATE, mario_update)
