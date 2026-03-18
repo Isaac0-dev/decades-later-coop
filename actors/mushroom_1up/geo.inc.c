003ea08798c3a98f912d444b8879a15bf2959123
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-18 14:43:30 -0700

    1up

---
+++ actors/mushroom_1up/geo.inc.c
@@ -0,0 +1,9 @@
+const GeoLayout mushroom_1up_geo[] = {
+    GEO_SHADOW(1, 180, 80),
+    GEO_OPEN_NODE(),
+        GEO_DISPLAY_LIST(4, dl_0302A660),
+        GEO_CLOSE_NODE(),
+    GEO_END(),
+};
+
+
