400557b1abff05528dcdae2d17bb5ae794032ceb
Fearl <40965214+Fearlv@users.noreply.github.com>
2026-03-19 08:38:56 -0500

    new cellHeightLimit and floorLowerLimit
    - ttc fixed

--- tweaks.lua
+++ tweaks.lua
@@ -7,6 +7,11 @@ gLevelValues.wingCapDurationTotwc = 1
 gLevelValues.vanishCapDurationVcutm = 1
 gLevelValues.metalCapDurationCotmc = 1
 
+gLevelValues.cellHeightLimit = 32767
+gLevelValues.floorLowerLimit = -32768
+gLevelValues.floorLowerLimitMisc = -32768
+gLevelValues.floorLowerLimitShadow = -32768
+
 gBehaviorValues.ToadStar1Requirement = 0 -- Overworld 2 Area 2 Brodute
 
 hook_event(HOOK_MARIO_UPDATE, function (m) m.numLives = 100 m.peakHeight = m.pos.y end)
