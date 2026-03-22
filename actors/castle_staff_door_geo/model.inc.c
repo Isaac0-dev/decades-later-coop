4c75efef2848267a3a31cb69a342bd4b59509f91
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-21 17:55:21 -0700

    staff door

---
+++ actors/castle_staff_door_geo/model.inc.c
@@ -0,0 +1,196 @@
+Lights1 door_seg3_lights_03009CE0 = gdSPDefLights1(0x3F, 0x3F, 0x3F, 0xFF, 0xFF, 0xFF, 0x28, 0x28, 0x28);
+
+Lights1 door_seg3_lights_03009CF8 = gdSPDefLights1(0x3F, 0x3F, 0x00, 0xFF, 0xFF, 0x00, 0x28, 0x28, 0x28);
+
+Texture door_seg3_texture_0300AD10[] = "actors/castle_staff_door_geo/door_seg3_texture_0300AD10";
+
+Texture door_seg3_texture_03009D10[] = "actors/castle_staff_door_geo/door_seg3_texture_03009D10";
+
+Texture door_seg3_texture_03012D10[] = "actors/castle_staff_door_geo/door_seg3_texture_03012D10";
+
+Vtx door_seg3_vertex_03013910[] = {
+    {{{-12, 0, -58}, 0x0, {0, 0}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-12, 0, 59}, 0x0, {990, 0}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-12, 1024, 59}, 0x0, {990, 2012}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-12, 1024, -58}, 0x0, {990, 2012}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{-12, 1024, 59}, 0x0, {0, 2012}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{604, 1024, 59}, 0x0, {0, 0}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{604, 1024, -58}, 0x0, {990, 0}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{604, 0, -58}, 0x0, {0, 0}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{-12, 0, 59}, 0x0, {990, 2012}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{-12, 0, -58}, 0x0, {0, 2012}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{604, 0, 59}, 0x0, {990, 0}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{604, 0, 59}, 0x0, {0, 0}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{604, 0, -58}, 0x0, {990, 0}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{604, 1024, -58}, 0x0, {990, 2012}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{604, 1024, 59}, 0x0, {0, 2012}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{-12, 1024, -58}, 0x0, {0, 2012}, {0x81, 0x00, 0x00, 0xFF}}},
+};
+
+Vtx door_seg3_vertex_03013A10[] = {
+    {{{604, 0, -58}, 0x0, {974, 1820}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-12, 0, -58}, 0x0, {0, 1816}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-12, 1024, -58}, 0x0, {0, 124}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{604, 1024, -58}, 0x0, {974, 128}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-12, 0, 59}, 0x0, {0, 1816}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{604, 0, 59}, 0x0, {974, 1812}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{604, 1024, 59}, 0x0, {974, 132}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{-12, 1024, 59}, 0x0, {0, 140}, {0x00, 0x00, 0x7F, 0xFF}}},
+};
+
+Vtx door_seg3_vertex_03013A90[] = {
+    {{{512, 512, 104}, 0x0, {0, 0}, {0xFF, 0x7E, 0x01, 0xFF}}},
+    {{{456, 432, 160}, 0x0, {0, 0}, {0xA6, 0x1F, 0x53, 0xFF}}},
+    {{{512, 488, 160}, 0x0, {0, 0}, {0x16, 0x49, 0x65, 0xFF}}},
+    {{{512, 372, 160}, 0x0, {0, 0}, {0xE7, 0xBA, 0x66, 0xFF}}},
+    {{{572, 432, 160}, 0x0, {0, 0}, {0x5A, 0xE3, 0x53, 0xFF}}},
+    {{{596, 432, 104}, 0x0, {0, 0}, {0x7E, 0x01, 0x01, 0xFF}}},
+    {{{512, 348, 104}, 0x0, {0, 0}, {0xFF, 0x82, 0x01, 0xFF}}},
+    {{{432, 432, 104}, 0x0, {0, 0}, {0x82, 0x01, 0x01, 0xFF}}},
+    {{{512, 488, 44}, 0x0, {0, 0}, {0xDA, 0x70, 0xD4, 0xFF}}},
+    {{{456, 432, 44}, 0x0, {0, 0}, {0x8F, 0xDE, 0xD4, 0xFF}}},
+    {{{512, 372, 44}, 0x0, {0, 0}, {0x24, 0x90, 0xD3, 0xFF}}},
+    {{{572, 432, 44}, 0x0, {0, 0}, {0x6F, 0x28, 0xD3, 0xFF}}},
+};
+
+Vtx door_seg3_vertex_03013B50[] = {
+    {{{512, 488, -156}, 0x0, {0, 0}, {0xE8, 0x48, 0x9B, 0xFF}}},
+    {{{512, 372, -156}, 0x0, {0, 0}, {0x17, 0xB9, 0x9A, 0xFF}}},
+    {{{456, 432, -156}, 0x0, {0, 0}, {0xA5, 0xE4, 0xAE, 0xFF}}},
+    {{{432, 432, -100}, 0x0, {0, 0}, {0x82, 0x01, 0xFF, 0xFF}}},
+    {{{512, 512, -100}, 0x0, {0, 0}, {0xFF, 0x7E, 0xFF, 0xFF}}},
+    {{{572, 432, -156}, 0x0, {0, 0}, {0x59, 0x20, 0xAC, 0xFF}}},
+    {{{512, 348, -100}, 0x0, {0, 0}, {0xFF, 0x82, 0xFF, 0xFF}}},
+    {{{596, 432, -100}, 0x0, {0, 0}, {0x7E, 0x01, 0xFF, 0xFF}}},
+    {{{572, 432, -40}, 0x0, {0, 0}, {0x70, 0xDC, 0x2D, 0xFF}}},
+    {{{512, 372, -40}, 0x0, {0, 0}, {0xD8, 0x91, 0x2D, 0xFF}}},
+    {{{456, 432, -40}, 0x0, {0, 0}, {0x90, 0x26, 0x2C, 0xFF}}},
+    {{{512, 488, -40}, 0x0, {0, 0}, {0x22, 0x71, 0x2C, 0xFF}}},
+};
+
+Vtx door_seg3_vertex_03014140[] = {
+    {{{441, 850, 64}, 0x0, {992, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{141, 850, 64}, 0x0, {0, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{141, 550, 64}, 0x0, {0, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{441, 550, 64}, 0x0, {992, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+};
+
+Vtx door_seg3_vertex_03014180[] = {
+    {{{441, 850, 59}, 0x0, {992, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{141, 850, 59}, 0x0, {0, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{141, 550, 59}, 0x0, {0, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{441, 550, 59}, 0x0, {992, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+};
+
+Gfx door_seg3_dl_03013C10[] = {
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, door_seg3_texture_0300AD10),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 2047, 256),
+    gsSPLight(&door_seg3_lights_03009CE0.l, 1),
+    gsSPLight(&door_seg3_lights_03009CE0.a, 2),
+    gsSPVertex(door_seg3_vertex_03013910 + 0, 16, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 3, 4, 5, 0x0),
+    gsSP2Triangles(3, 5, 6, 0x0, 7, 8, 9, 0x0),
+    gsSP2Triangles(7, 10, 8, 0x0, 11, 12, 13, 0x0),
+    gsSP2Triangles(11, 13, 14, 0x0, 0, 2, 15, 0x0),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, door_seg3_texture_03009D10),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 2047, 256),
+    gsSPVertex(door_seg3_vertex_03013A10 + 0, 8, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 0, 2, 3, 0x0),
+    gsSP2Triangles(4, 5, 6, 0x0, 4, 6, 7, 0x0),
+    gsSPEndDisplayList(),
+};
+
+Gfx door_seg3_dl_03013CC8[] = {
+    gsSPLight(&door_seg3_lights_03009CF8.l, 1),
+    gsSPLight(&door_seg3_lights_03009CF8.a, 2),
+    gsSPVertex(door_seg3_vertex_03013A90 + 0, 12, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 2, 3, 4, 0x0),
+    gsSP2Triangles(2, 1, 3, 0x0, 5, 2, 4, 0x0),
+    gsSP2Triangles(5, 0, 2, 0x0, 6, 4, 3, 0x0),
+    gsSP2Triangles(7, 6, 3, 0x0, 7, 3, 1, 0x0),
+    gsSP2Triangles(6, 5, 4, 0x0, 0, 7, 1, 0x0),
+    gsSP2Triangles(8, 9, 7, 0x0, 9, 6, 7, 0x0),
+    gsSP2Triangles(8, 7, 0, 0x0, 9, 10, 6, 0x0),
+    gsSP2Triangles(10, 5, 6, 0x0, 10, 11, 5, 0x0),
+    gsSP2Triangles(11, 0, 5, 0x0, 11, 8, 0, 0x0),
+    gsSPEndDisplayList(),
+};
+
+Gfx door_seg3_dl_03013D78[] = {
+    gsSPLight(&door_seg3_lights_03009CF8.l, 1),
+    gsSPLight(&door_seg3_lights_03009CF8.a, 2),
+    gsSPVertex(door_seg3_vertex_03013B50 + 0, 12, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 0, 2, 3, 0x0),
+    gsSP2Triangles(0, 3, 4, 0x0, 0, 5, 1, 0x0),
+    gsSP2Triangles(5, 0, 4, 0x0, 2, 1, 6, 0x0),
+    gsSP2Triangles(1, 7, 6, 0x0, 1, 5, 7, 0x0),
+    gsSP2Triangles(2, 6, 3, 0x0, 5, 4, 7, 0x0),
+    gsSP2Triangles(6, 8, 9, 0x0, 3, 6, 9, 0x0),
+    gsSP2Triangles(6, 7, 8, 0x0, 4, 3, 10, 0x0),
+    gsSP2Triangles(3, 9, 10, 0x0, 7, 4, 11, 0x0),
+    gsSP2Triangles(7, 11, 8, 0x0, 4, 10, 11, 0x0),
+    gsSPEndDisplayList(),
+};
+
+Gfx door_seg3_dl_03013E28[] = {
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_MODULATEI, G_CC_MODULATEI),
+    gsSPClearGeometryMode(G_SHADING_SMOOTH),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, 0, 0, 0, 0, 0, 0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, 0, 6, 0, 0, 5, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 124, 252),
+    gsSPDisplayList(door_seg3_dl_03013C10),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
+    gsSPSetGeometryMode(G_SHADING_SMOOTH),
+    gsSPDisplayList(door_seg3_dl_03013CC8),
+    gsSPDisplayList(door_seg3_dl_03013D78),
+    gsSPEndDisplayList(),
+};
+
+Gfx door_seg3_dl_030141C0[] = {
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_MODULATEIA, G_CC_MODULATEIA),
+    gsSPClearGeometryMode(G_SHADING_SMOOTH),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, 0, 0, 0, 0, 0, 0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 5, 0, G_TX_CLAMP, 5, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 124, 124),
+    gsSPLight(&door_seg3_lights_03009CE0.l, 1),
+    gsSPLight(&door_seg3_lights_03009CE0.a, 2),
+    gsSPEndDisplayList(),
+};
+
+Gfx door_seg3_dl_03014218[] = {
+    gsSP2Triangles(0, 1, 2, 0x0, 0, 2, 3, 0x0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
+    gsSPSetGeometryMode(G_SHADING_SMOOTH),
+    gsSPEndDisplayList(),
+};
+
+Gfx door_seg3_dl_03014310[] = {
+    gsSPDisplayList(door_seg3_dl_030141C0),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, door_seg3_texture_03012D10),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 1023, 256),
+    gsSPVertex(door_seg3_vertex_03014140 + 0, 4, 0),
+    gsSPBranchList(door_seg3_dl_03014218),
+};
+
+Gfx door_seg3_dl_03014340[] = {
+    gsSPDisplayList(door_seg3_dl_030141C0),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, door_seg3_texture_03012D10),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 1023, 256),
+    gsSPVertex(door_seg3_vertex_03014180 + 0, 4, 0),
+    gsSPBranchList(door_seg3_dl_03014218),
+};
+
