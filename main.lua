4a57a546bf1e5ac7d0fed1a4f155009cf3a7a453
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-23 11:03:30 -0700

    this disables boss dialog it seems, commented for now x.x

--- main.lua
+++ main.lua
@@ -6,13 +6,13 @@ require("data/a-bhv-loader")
 
 -- Bowser dialog
 
-function bowser_dialog(m)
+-- function bowser_dialog(m)
 
-    ---@type NetworkPlayer
-    local np = gNetworkPlayers[0]
-    if np.currLevelNum == LEVEL_BOWSER_1 or LEVEL_BOWSER_2 or LEVEL_BOWSER_3 and m.area.camera.cutscene == CUTSCENE_ENTER_BOWSER_ARENA then
-        m.area.camera.cutscene = 0
-    end
-end
+--     ---@type NetworkPlayer
+--     local np = gNetworkPlayers[0]
+--     if np.currLevelNum == LEVEL_BOWSER_1 or LEVEL_BOWSER_2 or LEVEL_BOWSER_3 and m.area.camera.cutscene == CUTSCENE_ENTER_BOWSER_ARENA then
+--         m.area.camera.cutscene = 0
+--     end
+-- end
 
-hook_event(HOOK_MARIO_UPDATE, bowser_dialog)
+-- hook_event(HOOK_MARIO_UPDATE, bowser_dialog)
