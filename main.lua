8f89f82daf581569e97c988f51d80b5159b08909
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-23 10:59:12 -0700

    no bowser dialog

--- main.lua
+++ main.lua
@@ -3,3 +3,16 @@
 -- incompatible: romhack
 
 require("data/a-bhv-loader")
+
+-- Bowser dialog
+
+function bowser_dialog(m)
+
+    ---@type NetworkPlayer
+    local np = gNetworkPlayers[0]
+    if np.currLevelNum == LEVEL_BOWSER_1 or LEVEL_BOWSER_2 or LEVEL_BOWSER_3 and m.area.camera.cutscene == CUTSCENE_ENTER_BOWSER_ARENA then
+        m.area.camera.cutscene = 0
+    end
+end
+
+hook_event(HOOK_MARIO_UPDATE, bowser_dialog)
