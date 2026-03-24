772f5b05d8f7eb663f0148fbb59894fea61d8659
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-24 12:27:38 -0700

    set positions

--- tweaks.lua
+++ tweaks.lua
@@ -24,4 +24,7 @@ vec3f_set(gLevelValues.starPositions.KoopaBobStarPos, 4665, 4503, 11006)
 vec3f_set(gLevelValues.starPositions.PssSlideStarPos, 15118, -7303, 212)
 vec3f_set(gLevelValues.starPositions.TuxieMotherStarPos, 366, -5336, 9379)
 vec3f_set(gLevelValues.starPositions.WigglerStarPos, 0, 400, 0)
+vec3f_set(gLevelValues.starPositions.GhostHuntBooStarPos, 150, -2210, -4260)
+vec3f_set(gLevelValues.starPositions.BalconyBooStarPos, 4090, 5060, -2360)
+
 gBehaviorValues.trajectories.KoopaBobTrajectory = get_trajectory('KoopaBoB_path')
