96cf27cc022173387391f5406fb3bba014d7790d
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-28 18:16:50 -0700

    ported even more tweaks
    some of these are prolly not needed if they are already spawned and stuff

--- tweaks/c-values.lua
+++ tweaks/c-values.lua
@@ -3,6 +3,19 @@ gLevelValues.exitCastleWarpNode = 10
 
 gServerSettings.skipIntro = 1
 
+gBehaviorValues.starsNeededForDialog.dialog1 = 10
+gBehaviorValues.starsNeededForDialog.dialog2 = 35
+gBehaviorValues.starsNeededForDialog.dialog3 = 80
+gBehaviorValues.starsNeededForDialog.dialog4 = 94
+gBehaviorValues.starsNeededForDialog.dialog5 = 141
+-- gBehaviorValues.starsNeededForDialog.dialog6 = idk if theres a 6th one
+
+gBehaviorValues.MipsStar1Requirement = 0
+gBehaviorValues.MipsStar2Requirement = 141
+gBehaviorValues.ToadStar1Requirement = 0
+gBehaviorValues.ToadStar2Requirement = 0
+gBehaviorValues.ToadStar3Requirement = 0
+
 gLevelValues.coinsRequiredForCoinStar = 80
 
 gLevelValues.pssSlideStarTime = 840
