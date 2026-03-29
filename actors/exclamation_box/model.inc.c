5529684c5e3cba26f5ffb7b4c8fd6f6235f918ca
ColbyRayz! <118496277+ColbyRayz@users.noreply.github.com>
2026-03-28 17:04:04 -0700

    exclamation box
    i get it you can see this texture already but this fixes the texture once a box is loaded instead of it using the vanilla texture, plus this possibly can be used later for the other box textures instead of all of them being white ? box

---
+++ actors/exclamation_box/model.inc.c
@@ -0,0 +1,206 @@
+Lights1 exclamation_box_seg8_lights_08012E10 = gdSPDefLights1(0x3F, 0x3F, 0x3F, 0xFF, 0xFF, 0xFF, 0x28, 0x28, 0x28);
+
+Texture exclamation_box_seg8_texture_08015E28[] = "actors/exclamation_box/exclamation_box_seg8_texture_08015E28";
+
+Texture exclamation_box_seg8_texture_08016628[] = "actors/exclamation_box/exclamation_box_seg8_texture_08016628";
+
+Texture exclamation_box_seg8_texture_08014628[] = "actors/exclamation_box/exclamation_box_seg8_texture_08014628";
+
+Texture exclamation_box_seg8_texture_08014E28[] = "actors/exclamation_box/exclamation_box_seg8_texture_08014E28";
+
+Texture exclamation_box_seg8_texture_08012E28[] = "actors/exclamation_box/exclamation_box_seg8_texture_08012E28";
+
+Texture exclamation_box_seg8_texture_08013628[] = "actors/exclamation_box/exclamation_box_seg8_texture_08013628";
+
+// Texture exclamation_box_seg8_texture_08017628[] = "actors/exclamation_box/exclamation_box_seg8_texture_08017628";
+
+// Texture exclamation_box_seg8_texture_08017E28[] = "actors/exclamation_box/exclamation_box_seg8_texture_08017E28";
+
+Vtx exclamation_box_seg8_vertex_08018E28[] = {
+    {{{-25, 1, -25}, 0x0, {992, 992}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-25, 52, -25}, 0x0, {992, 0}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{26, 52, -25}, 0x0, {0, 0}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-25, 1, 26}, 0x0, {0, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{26, 1, 26}, 0x0, {992, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{26, 52, 26}, 0x0, {992, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{-25, 52, 26}, 0x0, {0, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{26, 52, -25}, 0x0, {992, 0}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{26, 1, 26}, 0x0, {0, 992}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{26, 1, -25}, 0x0, {992, 992}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{26, 52, 26}, 0x0, {0, 0}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{-25, 1, -25}, 0x0, {0, 992}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-25, 1, 26}, 0x0, {992, 992}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-25, 52, 26}, 0x0, {992, 0}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-25, 52, -25}, 0x0, {0, 0}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{26, 1, -25}, 0x0, {0, 992}, {0x00, 0x00, 0x81, 0xFF}}},
+};
+
+Vtx exclamation_box_seg8_vertex_08018F28[] = {
+    {{{26, 1, -25}, 0x0, {992, 2014}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{26, 1, 26}, 0x0, {992, 0}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{-25, 1, 26}, 0x0, {0, 0}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{-25, 1, -25}, 0x0, {0, 2014}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{-25, 52, -25}, 0x0, {0, 0}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{-25, 52, 26}, 0x0, {0, 2014}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{26, 52, 26}, 0x0, {992, 2014}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{26, 52, -25}, 0x0, {992, 0}, {0x00, 0x7F, 0x00, 0xFF}}},
+};
+
+Vtx exclamation_box_seg8_vertex_080190A0[] = {
+    {{{-25, 1, 26}, 0x0, {992, 992}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-25, 52, 26}, 0x0, {992, 0}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{-25, 52, -25}, 0x0, {0, 0}, {0x81, 0x00, 0x00, 0xFF}}},
+    {{{26, 1, 26}, 0x0, {992, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{26, 52, 26}, 0x0, {992, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{-25, 52, 26}, 0x0, {0, 0}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{-25, 1, 26}, 0x0, {0, 992}, {0x00, 0x00, 0x7F, 0xFF}}},
+    {{{26, 1, -25}, 0x0, {992, 992}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{26, 52, -25}, 0x0, {992, 0}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{26, 52, 26}, 0x0, {0, 0}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{26, 1, 26}, 0x0, {0, 992}, {0x7F, 0x00, 0x00, 0xFF}}},
+    {{{-25, 1, -25}, 0x0, {992, 992}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{26, 52, -25}, 0x0, {0, 0}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{26, 1, -25}, 0x0, {0, 992}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-25, 52, -25}, 0x0, {992, 0}, {0x00, 0x00, 0x81, 0xFF}}},
+    {{{-25, 1, -25}, 0x0, {0, 992}, {0x81, 0x00, 0x00, 0xFF}}},
+};
+
+Vtx exclamation_box_seg8_vertex_080191A0[] = {
+    {{{-25, 52, 26}, 0x0, {0, 992}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{26, 52, 26}, 0x0, {2014, 992}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{26, 52, -25}, 0x0, {2014, 0}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{-25, 52, -25}, 0x0, {0, 0}, {0x00, 0x7F, 0x00, 0xFF}}},
+    {{{-25, 1, 26}, 0x0, {0, 0}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{-25, 1, -25}, 0x0, {0, 992}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{26, 1, -25}, 0x0, {2014, 992}, {0x00, 0x81, 0x00, 0xFF}}},
+    {{{26, 1, 26}, 0x0, {2014, 0}, {0x00, 0x81, 0x00, 0xFF}}},
+};
+
+Gfx exclamation_box_seg8_dl_08019058[] = {
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_MODULATEI, G_CC_MODULATEI),
+    gsSPClearGeometryMode(G_SHADING_SMOOTH),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, 0, 0, 0, 0, 0, 0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 5, 0, G_TX_CLAMP, 5, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 124, 124),
+    gsSPEndDisplayList(),
+};
+
+Gfx exclamation_box_seg8_dl_08018FA8[] = {
+    gsSPLight(&exclamation_box_seg8_lights_08012E10.l, 1),
+    gsSPLight(&exclamation_box_seg8_lights_08012E10.a, 2),
+    gsSPVertex(exclamation_box_seg8_vertex_08018E28 + 0, 16, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 3, 4, 5, 0x0),
+    gsSP2Triangles(3, 5, 6, 0x0, 7, 8, 9, 0x0),
+    gsSP2Triangles(7, 10, 8, 0x0, 11, 12, 13, 0x0),
+    gsSP2Triangles(11, 13, 14, 0x0, 0, 2, 15, 0x0),
+    gsSPEndDisplayList(),
+};
+
+Gfx exclamation_box_seg8_dl_08019008[] = {
+    gsSPVertex(exclamation_box_seg8_vertex_08018F28 + 0, 8, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 0, 2, 3, 0x0),
+    gsSP2Triangles(4, 5, 6, 0x0, 4, 6, 7, 0x0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
+    gsSPSetGeometryMode(G_SHADING_SMOOTH),
+    gsSPEndDisplayList(),
+};
+
+Gfx exclamation_box_seg8_dl_08019318[] = {
+    gsSPDisplayList(exclamation_box_seg8_dl_08019058),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08015E28),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 1023, 256),
+    gsSPDisplayList(exclamation_box_seg8_dl_08018FA8),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 6, 0, G_TX_CLAMP, 5, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 124, 252),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08016628),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 2047, 256),
+    gsSPBranchList(exclamation_box_seg8_dl_08019008),
+};
+
+Gfx exclamation_box_seg8_dl_080192D0[] = {
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_MODULATEI, G_CC_MODULATEI),
+    gsSPClearGeometryMode(G_SHADING_SMOOTH),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, 0, 0, 0, 0, 0, 0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 5, 0, G_TX_CLAMP, 5, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 124, 124),
+    gsSPEndDisplayList(),
+};
+
+Gfx exclamation_box_seg8_dl_08019220[] = {
+    gsSPLight(&exclamation_box_seg8_lights_08012E10.l, 1),
+    gsSPLight(&exclamation_box_seg8_lights_08012E10.a, 2),
+    gsSPVertex(exclamation_box_seg8_vertex_080190A0 + 0, 16, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 3, 4, 5, 0x0),
+    gsSP2Triangles(3, 5, 6, 0x0, 7, 8, 9, 0x0),
+    gsSP2Triangles(7, 9, 10, 0x0, 11, 12, 13, 0x0),
+    gsSP2Triangles(11, 14, 12, 0x0, 0, 2, 15, 0x0),
+    gsSPEndDisplayList(),
+};
+
+Gfx exclamation_box_seg8_dl_08019280[] = {
+    gsSPVertex(exclamation_box_seg8_vertex_080191A0 + 0, 8, 0),
+    gsSP2Triangles(0, 1, 2, 0x0, 0, 2, 3, 0x0),
+    gsSP2Triangles(4, 5, 6, 0x0, 4, 6, 7, 0x0),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_OFF),
+    gsDPPipeSync(),
+    gsDPSetCombineMode(G_CC_SHADE, G_CC_SHADE),
+    gsSPSetGeometryMode(G_SHADING_SMOOTH),
+    gsSPEndDisplayList(),
+};
+
+Gfx exclamation_box_seg8_dl_08019378[] = {
+    gsSPDisplayList(exclamation_box_seg8_dl_080192D0),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08014628),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 1023, 256),
+    gsSPDisplayList(exclamation_box_seg8_dl_08019220),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 5, 0, G_TX_CLAMP, 6, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 252, 124),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08014E28),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 2047, 128),
+    gsSPBranchList(exclamation_box_seg8_dl_08019280),
+};
+
+Gfx exclamation_box_seg8_dl_080193D8[] = {
+    gsSPDisplayList(exclamation_box_seg8_dl_08019058),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08012E28),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 1023, 256),
+    gsSPDisplayList(exclamation_box_seg8_dl_08018FA8),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 8, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 6, 0, G_TX_CLAMP, 5, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 124, 252),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08013628),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 2047, 256),
+    gsSPBranchList(exclamation_box_seg8_dl_08019008),
+};
+
+Gfx exclamation_box_seg8_dl_08019438[] = {
+    gsSPDisplayList(exclamation_box_seg8_dl_080192D0),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08017628),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 1023, 256),
+    gsSPDisplayList(exclamation_box_seg8_dl_08019220),
+    gsDPTileSync(),
+    gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 16, 0, G_TX_RENDERTILE, 0, G_TX_CLAMP, 5, 0, G_TX_CLAMP, 6, 0),
+    gsDPSetTileSize(G_TX_RENDERTILE, 0, 0, 252, 124),
+    gsDPSetTextureImage(G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, exclamation_box_seg8_texture_08017E28),
+    gsDPLoadSync(),
+    gsDPLoadBlock(G_TX_LOADTILE, 0, 0, 2047, 128),
+    gsSPBranchList(exclamation_box_seg8_dl_08019280),
+};
+
