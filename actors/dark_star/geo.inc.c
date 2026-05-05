c6b5b3c58d6f7f655ddeed458202d315df63d386
ColbyRayz! <colbyray7827@gmail.com>
2026-05-05 15:42:35 -0700

    dark star and lil fix for box states
    certified isaac moment
    thanks

---
+++ actors/dark_star/geo.inc.c
@@ -0,0 +1,7 @@
+const GeoLayout dark_star_geo[] = {
+    GEO_CULLING_RADIUS(1500),
+    GEO_OPEN_NODE(),
+        GEO_DISPLAY_LIST(LAYER_OPAQUE, ccm_dl_0701DAE0),
+    GEO_CLOSE_NODE(),
+    GEO_END(),
+};
--- data/c-exclamation-box.lua
+++ data/c-exclamation-box.lua
@@ -1,5 +1,5 @@
 -- Allow exclamation boxes to load the extra states
 hook_behavior(id_bhvExclamationBox, OBJ_LIST_SURFACE, false, function (o)
-    if o.oBehParams2ndByte < 3 then return end -- It's a normal exclamation box
+    if o.oBehParams2ndByte < 4 then return end -- It's a normal exclamation box
     o.oAnimState = (o.oBehParams & 0xF) - 1
 end, nil)
